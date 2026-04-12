783584ca813d631ed50e531c9b1787c46892cde7
Abdulahi Hassen <abdala.baaji@gmail.com>
2026-04-12 16:24:58 +0300

    updated code

---
+++ Lab Practice/Lab 9 Next.js React Pages and Components/Startup Code/my-finance-app/app/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+
+      <h1>Hello {2 + 7} </h1>
+      <h2>Welcome to CMPS-350</h2>
+    </div>
+  );
+}
+
+// Rules of JSX
+
+// You can have only one root element
